5d3b16b6ac4e09ebf15c27dc7f9f1b09b048b06d
powermate <218421152+powermate25@users.noreply.github.com>
2025-10-04 23:45:05 +0000

    Checkpoint save: EaS project done Added option for random colored grid.

--- script.js
+++ script.js
@@ -50,23 +50,57 @@ btnGenerateGrid.addEventListener(
         i.style.backgroundColor = "yellow"
     }
 ) ) 
-/* gridContainer.style.border = "1px solid rgba(255, 195, 45, 1)" */
 
 } 
 );
 
 
+// random color generator
+
+const btnGenerateGridRandomColor = document.querySelector("#btn-generate-grid2")
+btnGenerateGridRandomColor.addEventListener(
+    "click", () => {
+        // step 1: select grid container
+        const gridContainer = document.querySelector("#main-grid-container")
+
+        // step 2: initialize container (clear old divs) 
+        gridContainer.replaceChildren("")
+        
+
+        // step 3: execute createGrids function with user configuration and add general class to new created divs
+        createGrids2(prompt("How many grid per line?", Number(16)))
+
+        // Step 4: indexing new divs by class created above 
+        const gridItems = document.querySelectorAll(".grid-items")
+        
+        // step 5: add event lister to each grid item using for each loop
+        gridItems.forEach(
+    i => i.addEventListener(
+    "mouseenter", () => {
+        i.style.backgroundColor = `#${Math.floor(Math.random()*1000000)}` 
+       for (let x = 0; x <= 10; x++){
+        let newColor = Math.floor(Math.random()*100000)
+        let currentColor = newColor
+        i.addEventListener("mouseenter", () => { i.style.backgroundColor = `#${newColor}` })
+        }
+    }
+) )
+
+} 
+);
+
+// ${Math.floor(Math.random()*100000)}
+
 function createGrids2(userInput){
     let gridNumber = userInput * userInput
     if (userInput > 100){ return createGrids2(prompt("Please enter a number below or equal to 100", Number(16))) }
     else {
-    // let gridContainer = document.querySelector("#main-grid-container")
+    
     for(let i = 1; i <= gridNumber; i++){
         const createGrid = document.createElement("div")
         createGrid.classList.add("grid-items")
         gridContainer.append(createGrid)
         gridContainer.style.cssText = `width: 400px; height: 400px;`
-       // gridContainer.style.border = "1px solid rgba(255, 195, 45, 1)"
        const gridItems = document.querySelectorAll(".grid-items");
     gridItems.forEach(
     i => i.style.cssText = ` width: ${100 / userInput}%; height: ${100 / userInput}%`
@@ -74,7 +108,7 @@ function createGrids2(userInput){
          
     } }
 }
-  
+ 
 // createGrids2(16)   
 
 /* gridItems2.forEach(
@@ -84,3 +118,26 @@ function createGrids2(userInput){
         i.style.cssText = " width: ; height: ;"
     }
 ) ) */
+let current = Math.floor(Math.random()*100000)
+    console.log(
+        current,
+        current,
+        current,
+        current,
+        current, 
+    )
+    
+    
+/* gridItems.forEach(
+    i => i.addEventListener(
+    "mouseenter", () => {
+        i.style.backgroundColor = "yellow"
+       for (let x = 0; x <= 10; x++){
+        let newColor = Math.floor(Math.random()*100000)
+        let currentColor = newColor
+        if(i.style.backgroundColor = `yellow`) {
+        i.addEventListener("mouseenter", () => { i.style.backgroundColor = `#${currentColor}` })
+        }
+        }
+    }
+) )  */
